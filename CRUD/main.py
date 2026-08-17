fc01d9881c65d56284e542dac438e04ff81ef2ae
Muhiu Din <muhiudin005@gmail.com>
2026-08-17 19:01:31 +0500

    root and health added

--- CRUD/main.py
+++ CRUD/main.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"name": "Task API", "version": "1.0.0" ,"endpoints": ["/tasks"]}
 
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
